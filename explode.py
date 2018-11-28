ef8b78c7e62ad82e754324dfc2ecec25a52f65b5
pawarit28 <39638268+pawarit28@users.noreply.github.com>
2018-11-28 15:55:19 +0100

    Update explode.py

--- explode.py
+++ explode.py
@@ -1,4 +1,5 @@
 def explode(df, explode_column):
+    
     """ This function explodes a column (whose elements are Python lists) in a similar
         manner to the equivalent Spark explode method """
     
@@ -6,7 +7,7 @@ def explode(df, explode_column):
     df['explode_id'] = df.index
     
     stacked_df = pd.DataFrame(df[explode_column].tolist()).stack().reset_index()
-    stacked_df.columns = ['explode_id', 'explode_name', explode_column]    
+    stacked_df.columns = ['explode_id', 'explode_label', explode_column]    
 
     df = df.drop(columns=[explode_column])
-    return df.merge(stacked_df, on='explode_id').drop(columns=['explode_id', 'explode_name'])
+    return df.merge(stacked_df, on='explode_id').drop(columns=['explode_id', 'explode_label'])
