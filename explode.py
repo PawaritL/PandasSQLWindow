db0688312488119aec298a0d95f5e83b595fc200
pawarit28 <39638268+pawarit28@users.noreply.github.com>
2018-11-28 15:48:53 +0100

    Add files via upload

---
+++ explode.py
@@ -0,0 +1,9 @@
+def explode(df, explode_column):
+    df = df.reset_index(drop=True)
+    df['explode_id'] = df.index
+    
+    stacked_df = pd.DataFrame(df[explode_column].tolist()).stack().reset_index()
+    stacked_df.columns = ['explode_id', 'explode_name', explode_column]    
+
+    df = df.drop(columns=[explode_column])
+    return df.merge(stacked_df, on='explode_id').drop(columns=['explode_id', 'explode_name'])
